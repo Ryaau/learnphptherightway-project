acdcb835990e9801019e7eb8753d95a8aea1c237
zephyre <yoworuy@gmail.com>
2024-03-28 10:44:29 +0500

    make whole dto class readonly

--- app/DTO/EmailValidationResult.php
+++ app/DTO/EmailValidationResult.php
@@ -4,9 +4,9 @@ declare(strict_types = 1);
 
 namespace App\DTO;
 
-class EmailValidationResult
+readonly class EmailValidationResult
 {
-    public function __construct(public readonly int $score, public readonly bool $isDeliverable)
+    public function __construct(public int $score, public bool $isDeliverable)
     {
     }
 }
